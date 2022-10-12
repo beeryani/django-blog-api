466d64f2955f0e73543cfcc9834e89c7efa4d653
pratyush <guptapratyush98@gmail.com>
2022-10-13 00:40:43 +0530

    - add Blog Detail View & URL

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "blog_app"
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
+    path("<int:pk>/", views.BlogDetailView.as_view(), name="blog"),
 ]

a6a13be88c810de4625aa9ca73301aca245d8a18
pratyush <guptapratyush98@gmail.com>
2022-10-18 07:31:27 +0530

    - Update form class in views

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
 
 
 class HomeView(ListView):
@@ -14,5 +15,5 @@ class BlogDetailView(DetailView):
 
 class AddBlogDetailView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = "add_blog.html"
-    fields = ('title', 'body')

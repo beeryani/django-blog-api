afe9ecd466ec6fbd3b7f7313fd8fc5bccf76cc30
pratyush-eightfold <pratyush.gupta@eightfold.ai>
2023-02-25 14:30:28 +0530

    - path to add React application added

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = "blog_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "blog/templates/blog")],
+        "DIRS": [os.path.join(BASE_DIR, "django-blog-frontend/dist")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -116,7 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "assets/"
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "django-blog-frontend/dist/assets")]
+
+MEDIA_URL = "dist/"
+
+MEDIA_ROOT = [os.path.join(BASE_DIR, "django-blog-frontend/dist")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- blog_project/urls.py
+++ blog_project/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("blog.urls")),
+    path("", TemplateView.as_view(template_name="index.html")),
 ]

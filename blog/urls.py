19efdc3fbbcb9a9e205715f20c62839f6217c598
pratyush-eightfold <pratyush.gupta@eightfold.ai>
2023-02-25 14:58:15 +0530

    - remove boilerplate template Blog app

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "blog_app"
-urlpatterns = [
-    path("", views.HomeView.as_view(), name="home"),
-    path("<int:pk>/blog/", views.BlogDetailView.as_view(), name="blog"),
-    path("add_blog/", views.AddBlogDetailView.as_view(), name="add_blog"),
-]

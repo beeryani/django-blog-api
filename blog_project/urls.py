917d4698248dcf2db546f2876f89fd4fea76776d
Pratyush <pratyush@Pratyushs-MacBook-Air.local>
2023-03-05 03:16:34 +0530

    - configure static file paths | update settings

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -5,4 +5,4 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("blog_backend.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), path("", include("blog_backend.urls"))]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

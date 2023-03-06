329b2f6b362e5c31856fe58bb7bf81c6044771e2
Pratyush <pratyush@Pratyushs-MacBook-Air.local>
2023-03-06 15:44:31 +0530

    - update paths to API endpoints

--- blog_backend/urls.py
+++ blog_backend/urls.py
@@ -3,8 +3,12 @@ from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
+    path("", views.blogPostOverview),
     path("blog/", views.BlogPostListView.as_view()),
+    path("createblog/", views.BlogPostCreateBlog.as_view()),
     path("blog/<int:pk>/", views.BlogPostDetailedView.as_view()),
+    path("updateblog/<int:pk>/", views.BlogPostUpdateView.as_view()),
+    path("deleteblog/<int:pk>/", views.BlogPostDeleteView.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)

fb3e07a01f614c757c979bdfca64b9b562d1ff1d
Pratyush Gupta <guptapratyush98@gmail.com>
2023-03-19 19:42:41 +0530

    - update base settings

--- settings/base.py
+++ settings/base.py
@@ -32,7 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS_BASE")
 
 
 # Application definition
